4a296c8ad4f324ca2070d546e84c0bf0fa9ab901
= <=>
2016-03-15 09:08:33 -0300

    adicionando arquivo bastardo

---
+++ WebApi/WebApi/Models/Cliente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class Cliente
+    {
+        [Key]
+        public int IDCliente { get; set; }
+        public string Nome { get; set; }
+
+        //TODO: Possivel Refactoring
+        public string Endereco { get; set; }
+
+        public byte[] Imagem { get; set; }
+    }
+}
